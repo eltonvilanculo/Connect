8f8fd888930d0d08f6741429c26ace6bb2aafb8c
h <eltonvillas10@gmail.com>
2019-05-05 17:32:06 +0200

    Login e registro(completo)

--- app/src/main/java/com/example/connect/ContentActivity.java
+++ app/src/main/java/com/example/connect/ContentActivity.java
@@ -4,18 +4,30 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class ContentActivity extends AppCompatActivity {
+
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
